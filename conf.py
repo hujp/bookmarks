ec166ee39faf6faafa4434788f615322e35e4bea
hujp <hujp1985@foxmail.com>
2020-10-22 10:56:36 +0800

    update copyright

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'bookmarks'
-copyright = '2020, hujp'
+copyright = '2021, hujp'
 author = 'hujp'
 
 # The full version, including alpha/beta/rc tags
